136d2726e452b8d3590908b5a0074d955f12d6e0
Daisssy <nding120@gmail.com>
2024-06-18 21:20:41 -0400

    2 nav-bars css updates

--- components/bottomBar-actions.tsx
+++ components/bottomBar-actions.tsx
@@ -1,32 +1,94 @@
-'use client';
+"use client";
 
 import useCart from "@/hooks/use-cart";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./ui/button";
 import { ShoppingCart } from "lucide-react";
 import { Search } from "lucide-react";
 import { Cigarette } from "lucide-react";
+import { Filter } from "lucide-react";
+import { cn } from "@/lib/util";
 
 const BottomBarActions = () => {
   const router = useRouter();
   const cart = useCart();
+  const [isClicked, setIsClicked] = useState("");
+
+  const isClickHandler = (v: string) => {
+    setIsClicked(v);
+  };
   return (
     <div className="w-full flex justify-between items-center gap-x-4">
-      <Button className="flex flex-col items-center rounded-full bg-white px-4 py-2">
-      <Search color="black"/>
-        <span className="ml-2 text-sm font-medium text-black">Search</span>
+      <Button
+        className="flex flex-col items-center rounded-full bg-white px-4 py-2 font-normal"
+        onClick={() => isClickHandler("search")}
+      >
+        <Search
+          color="black"
+          strokeWidth={isClicked === "search" ? 2.5 : 1.25}
+        />
+        <span
+          className={cn(
+            "ml-2 text-sm text-black",
+            isClicked === "search" ? "font-bold" : "font-normal"
+          )}
+        >
+          Search
+        </span>
       </Button>
-      <Button className="flex flex-col items-center rounded-full bg-white px-4 py-2">
-      <Cigarette color="black"/>
-        <span className="ml-2 text-sm font-medium text-black">Other</span>
+
+      <Button
+        className="flex flex-col items-center rounded-full bg-white px-4 py-2 font-normal"
+        onClick={() => isClickHandler("filter")}
+      >
+        <Filter
+          strokeWidth={isClicked === "filter" ? 2.5 : 1.25}
+          color="black"
+        />
+        <span
+          className={cn(
+            "ml-2 text-sm text-black",
+            isClicked === "filter" ? "font-bold" : "font-normal"
+          )}
+        >
+          Filter
+        </span>
       </Button>
       <Button
-        onClick={() => router.push("/cart")}
-        className="flex flex-col items-center rounded-full bg-white px-4 py-2 min-w-16"
+        className="flex flex-col items-center rounded-full bg-white px-4 py-2 font-normal"
+        onClick={() => isClickHandler("orders")}
       >
-        <ShoppingCart color="black"/>
-        <span className="ml-2 text-sm font-medium text-black">
+        <Cigarette
+          color="black"
+          strokeWidth={isClicked === "orders" ? 2.5 : 1.25}
+        />
+        <span
+          className={cn(
+            "ml-2 text-sm text-black",
+            isClicked === "orders" ? "font-bold" : "font-normal"
+          )}
+        >
+          Orders
+        </span>
+      </Button>
+      <Button
+        onClick={() => {
+          isClickHandler("cart");
+          router.push("/cart");
+        }}
+        className="flex flex-col items-center rounded-full bg-white px-4 py-2 min-w-16 font-normal"
+      >
+        <ShoppingCart
+          color="black"
+          strokeWidth={isClicked === "cart" ? 2.5 : 1.25}
+        />
+        <span
+          className={cn(
+            "ml-2 text-sm text-black",
+            isClicked === "cart" ? "font-bold" : "font-normal"
+          )}
+        >
           {cart.items.length}
         </span>
       </Button>
--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -8,6 +8,7 @@ import { User } from "lucide-react";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const [isClicked, setIsClicked] = useState("");
 
   useEffect(() => {
     setIsMounted(true);
@@ -16,16 +17,23 @@ const NavbarActions = () => {
   const router = useRouter();
   const cart = useCart();
 
+  const isClickHandler = (v: string) => {
+    setIsClicked(v);
+  };
+
   if (!isMounted) {
     return null;
   }
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button
-        onClick={() => router.push("/user")}
+        onClick={() => {
+          isClickHandler("user");
+          router.push("/user");
+        }}
         className="flex items-center rounded-full bg-white px-4 py-2"
       >
-        <User color="black"/>
+        <User color="black" strokeWidth={isClicked === "user" ? 2.5 : 1.25}/>
         {/* <span className="ml-2 text-sm font-medium text-white">User</span> */}
       </Button>
     </div>
